0a48397e4dcc7f3a6b0bf6dc9118547b8718eb87
habertrogena <habertorogy12@gmail.com>
2024-02-10 17:46:24 +0300

    creates list rating component

--- app/components/index.ts
+++ app/components/index.ts
@@ -3,4 +3,5 @@ export * from './footer';
 export * from './container';
 export * from './homebanner';
 export * from './products';
-export * from './button'
+export * from './button';
+export * from './heading';
--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -3,6 +3,7 @@ import { Container } from '@/app/components/container/Container'
 import React from 'react'
 import { ProductDetails } from '../ProductDetails'
 import { product, products } from '@/utils'
+import { ListRating } from './listRating'
 
 interface Iparams{
     productId?:string
@@ -16,6 +17,10 @@ export default function Product({params}:{params:Iparams}) {
     <div className='p-8'>
         <Container>
             <ProductDetails product={product} />
+            <div className='flex flex-col mt-20 gap-4'>
+              <div> Add rating</div>
+              <ListRating product={product}/>
+            </div>
         </Container>
     </div>
   )
